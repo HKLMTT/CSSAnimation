ab09f23bee1e59bf934de1a34e76c2ba0e363027
HKLMTT <liujia307@qq.com>
2017-04-21 00:10:48 +0800

    Update ca.js

--- ES6/ca.js
+++ ES6/ca.js
@@ -3,8 +3,22 @@
  * */
 "use strict";
 (function($) {
-    $.fn.ca_v = "0.0.1";
-    
+    $.ca = {
+        getThis : function (a) {
+            var t;
+            a.each(function () {
+                if(this.anim) {
+                    t = this;
+                    return;
+                }
+            })
+            return t ? t : a[0];
+        },
+        get v(){
+            return "0.0.24";
+        }
+    };
+    console.log("init ca_v." + $.ca.v);
     var Browser = getBrowser();
     var transitions = {
         'transition'        : 'transitionend',
@@ -12,46 +26,114 @@
         '-webkit-transition': 'webkitTransitionEnd'
     }
     var noanim      = {"perspective-origin":true,"transition":true};
-    var aliasList   = {"x":"translateX","y":"translateY","z":"translateZ","origin":"perspective-origin","alpha":"opacity"};
-    $.fn.ca = function (ops , callback , step) {
-        if(this[0].animList instanceof Array === false) this[0].animList = [];
+    var aliasList   = {"x":"translateX","y":"translateY","z":"translateZ","pOrigin":"perspective-origin","tOrigin":"transform-origin","alpha":"opacity"};
+    $.fn.ca = function (ops , callback , step) {//
+        var _this = $.ca.getThis(this);
+        if(_this.animList instanceof Array === false) _this.animList = [];
         var run = function (e) {
             if(e === "complete") {
-                this[0].animList.shift();
-                if(this[0].anim) {
-                    this[0].anim.clear();
-                    this[0].anim = null;
-                    delete this[0].anim;
+                if(_this.anim.hide) this.hide(0);
+                if(_this.animList[0][1]) {
+                    var complete = _this.animList[0][1];
+                    setTimeout(() => complete.call(this , e), 0);
+                }
+                _this.animList.shift();
+                if(_this.anim) {
+                    _this.anim.clear();
+                    _this.anim = null;
+                    delete _this.anim;
                 }
             }
-            if(this[0].animList.length === 0) {
-                delete this[0].animList;
-                if(callback) callback(e);
+            if(_this.animList && _this.animList.length > 0) {
+                _this.anim = new CA(this , _this.animList[0][0] , (e) => run.call(this , e) , step );
+                window.setTimeout(() => _this.anim && _this.anim.run() , 0);
             }else {
-                this[0].anim = new CA(this , this[0].animList[0] , (e) => run.call(this , e) , step );
-                setTimeout(() => this[0].anim.run() , 0);
+                this.css("transition" , "");
+                if(_this.animList) delete _this.animList;
             }
         }
-        this[0].animList.push(ops);
-        if(this[0].animList.length === 1) run.call(this);
+        _this.animList.push(arguments);
+        if(_this.animList.length === 1) run.call(this);
         return this;
     }
-    $.fn.caplay     = function () {
-        this[0].anim.play();
+    $.fn.caPlay     = function () {
+        if(this[0].anim) this[0].anim.play();
         return this;
     }
-    $.fn.castop     = function () {
-        this[0].anim.stop();
+    $.fn.caStop     = function () {
+        if(this[0].anim) {
+            this[0].anim.stop();
+            this.caClear();
+        }
         return this;
     }
-    $.fn.capause    = function () {
-        this[0].anim.pause();
+    $.fn.caPause    = function () {
+        if(this[0].anim) this[0].anim.pause();
+        return this;
+    }
+    $.fn.caClear    = function () {
+        this.each(function () {
+            if(this.anim) {
+                this.anim.clear();
+                this.anim = null;
+                this.animList = null;
+                delete this.animList;
+                delete this.anim;
+            }
+        })
+        return this;
+    }
+    $.fn.caX        = function () {
+        return CA.getTranslate3d(this).x;
+    }
+    $.fn.caY        = function () {
+        return CA.getTranslate3d(this).y;
+    }
+    $.fn.caZ        = function () {
+        return CA.getTranslate3d(this).z;
+    }
+    $.fn.ca3D        = function () {
+        return CA.getTranslate3d(this);
+    }
+    //折叠动画
+    $.fn.caSlide    = function (b, t, callback, type) {
+        if(!type) type = "slide";
+        var an = `ca-${type}`;
+        if(typeof b !== "boolean") {
+            if(this.attr(an) === "hide" || this.css("display") === "none") b = false;
+            else b = true;
+        }else {
+            b = !b;
+        }
+        if(t === undefined || isNaN(t)) t = 400;
+        if(type === "flip"){
+            this.css("transform-origin", "50% 50% 0");
+            if(b) this.attr(an, "hide").caClear().ca({rotateX:90, alpha:0, hide:true, time:t}, callback);
+            else this.attr(an, "show").caClear().ca({rotateX:90, alpha:0, hide:true, time:0}).ca({rotateX:0, alpha:1, time:t}, callback);
+        }else if(type === "turn"){
+            this.css("transform-origin", "50% 50% 0");
+            if(b) this.attr(an, "hide").caClear().ca({rotateY:90, alpha:0, hide:true, time:t}, callback);
+            else this.attr(an, "show").caClear().ca({rotateY:90, alpha:0, hide:true, time:0}).ca({rotateY:0, alpha:1, time:t}, callback);
+        }else {
+            this.css("transform-origin", "0 0");
+            if(b) this.attr(an, "hide").caClear().ca({scaleY:0, hide:true, time:t}, callback);
+            else this.attr(an, "show").caClear().ca({scaleY:0, alpha:1, hide:true, time:0}).ca({scaleY:1,time:t}, callback);
+        }
+        return this;
+    }
+    //翻转动画
+    $.fn.caFlip     = function(b, t, callback) {
+        this.caSlide(b , t === undefined ? 300 : t, callback, "flip");
+        return this;
+    }
+    $.fn.caTurn     = function (b, t, callback) {
+        this.caSlide(b , t === undefined ? 300 : t, callback, "turn");
         return this;
     }
     class CA {
         constructor(){
             if(typeof arguments[0] != "object" || this.state === "pause") return ;
-            
+
             this.state      = "init";
             this.transform  = {};
             this.obj        = arguments[0];
@@ -60,50 +142,67 @@
             this.ease       = 'ease';//贝赛尔曲线
             this.count      = 0;//动画循环次数
             this.reanim     = false;//是否显示反向动画 当count不等于0时生效
+            this.hide       = false;//动画结束后是否隐藏
             this.css        = this.arrange(arguments[1]);
-            this.complete   = typeof arguments[2] === "function" ? arguments[2] : null;
-            this.steping    = typeof arguments[3] === "function" ? arguments[3] : null;
+            this.complete   = arguments[2];
+            this.steping    = arguments[3];
             //this.run();
         }
         //执行动画
         run(e){
-            if(this.state === "pause") return;
+            if(this.state === "pause" || this.obj === undefined || this.css  === undefined) return;
             this.state = "playing";
-            if(e != "re") this.css[`transition`] = this.getTransition(this.time , this.ease , this.delay);
-            this.obj.css(this.css);
-            this.step(true);
-            this.obj.first().one(transitions[`${Browser}transition`] , (e)=> this.end.apply(this , arguments));
+            if(e != "re" && this.time > 0) this.css[`transition`] = this.getTransition(this.time , this.ease , this.delay);
+            if(this.timeout) clearTimeout(this.timeout);
+            this.obj.show(0);
+            //debugger;
+            if(this.time === 0 && this.delay === 0) {
+                this.obj.css(this.css)
+                this.end.call(this , "complete");
+            }else{
+                if(this.time > 0) {
+                    this.step(true);
+                    this.obj.first().one(transitions[`${Browser}transition`], () => this.end.call(this, "complete"));
+                    this.timeout = setTimeout(()=> this.end.call(this , "complete") , (this.time + this.delay) * 1.2);
+                    this.obj.css(this.css)
+                }else {
+                    this.timeout = setTimeout(()=> {
+                        this.obj.css(this.css)
+                        this.end.call(this , "complete");
+                    } , (this.time + this.delay) * 1.2);
+                }
+            }
         }
         //动画记步
         step(b){
             clearInterval(this.Interval);
             if(b) {
                 this.starTime = new Date();
-                this.Interval = setInterval(() => {if(this.steping) this.steping();} , 1000 / 30);
+                if(this.steping) this.Interval = setInterval(() => {this.steping();} , 1000 / 30);
             }
         }
         //拼接Transition
         getTransition(time , ease , delay){
             var transition = "";
-            for (var k in this.css) {
-                if(noanim[k]) continue;
-                if (transition != "") transition += ",";
-                transition += `${k} ${time}ms ${ease}` + (delay > 0 ? ` ${delay}ms` : "");
+            if(time > 0) {
+                for (var k in this.css) {
+                    if (noanim[k] || this.css[k] == "") continue;
+                    if (transition != "") transition += ",";
+                    transition += `${k} ${time}ms ${ease}` + (delay > 0 ? ` ${delay}ms` : "");
+                }
             }
             return transition;
         }
         //重置
         reset(callback){
-            this.obj.css(this.lastcss);
             if(this.reanim) {
                 this.step(true);
-                this.obj.first().one(transitions[`${Browser}transition`] , ()=>{
-                    callback.apply(this , arguments);
-                });
+                this.obj.first().one(transitions[`${Browser}transition`] , () => callback.call(this));
             }else{
                 this.obj.css("transition" , "");
                 window.setTimeout(() => callback.call(this) , 0);
             }
+            this.obj.css(this.lastcss);
         }
         //播放动画
         play(){
@@ -134,27 +233,29 @@
         stop(){
             if(this.state === "init") return;
             this.state = "stop";
-            this.obj.css("transition" , "");
+            if(this.obj) this.obj.css("transition" , "");
             this.clear();
         }
         //动画完成  complete(事件,剩余执行次数)
         end(){
             this.step(false);
-            if(this.count === 0) {
-                this.obj.css("transition" , "");
+            if(this.timeout) clearTimeout(this.timeout);
+            if(!this.obj) return;
+            this.obj.off(transitions[`${Browser}transition`]);
+            if(this.count === 0 || isNaN(this.count)) {
                 this.state = "end";
-                if(this.complete) window.setTimeout(() => this.complete("complete" , 0) , 0);
-                return ;
+                this.complete(arguments[0] , 0);
+            }else{
+                if(this.steping) this.steping("replay" , this.count);
+                this.count--;
+                this.reset(() => this.run("re"));
             }
-            if(this.steping) this.steping("replay" , this.count);
-            this.count--;
-            this.reset(()=>this.run("re"));
         }
         //整理CSS
-        arrange(ops){
+        arrange(ops) {
             var o = {"transform" : {}};//新建样式
             this.lastcss = {}; //备份初始样式
-            this.oTf(this.obj);//转换transform
+            this.oTf(this.obj);//转换初始transform
             for (var k in ops) {
                 switch (k) {
                     case "time" :
@@ -171,11 +272,13 @@
                         if(!isNaN(ops[k])) this.count = ops[k];
                         break;
                     case "reanim" :
-                        if(!isNaN(ops[k])) this.reanim = ops[k];
+                        if(typeof ops[k] === "boolean") this.reanim = ops[k];
+                        break;
+                    case "hide" :
+                        if(typeof ops[k] === "boolean") this.hide = ops[k];
                         break;
                     default :
                         var css = this.alias(k);
-                        
                         if(typeof this[css] === "function") {
                             if(ops[k] instanceof Array) { // [ ... ] array
                                 this[css](ops[k]);
@@ -183,7 +286,7 @@
                                 this[css]([ops[k]]);
                             }
                         } else {
-                            o[css] = ops[k];
+                            o[css] = ops[k].toString();
                             this.lastcss[css] = this.obj.css(css);
                         }
                 }
@@ -206,31 +309,33 @@
                 this[k](f[k]);
             });
         }
-        
+
         alias(s){
             var ss = aliasList[s];
             return typeof ss === "string" ? ss : s;
         }
-        
+
         setTf(args , name , defaults) {
             //if(args instanceof Array === false) return;
             if(!this.transform[name]) this.transform[name] = defaults;
             if(args instanceof Array) {
                 for(var i in args) {
-                    if(args[i]) this.transform[name][i] = args[i];
+                    if(args[i] !== null) this.transform[name][i] = args[i];
                 }
             }else{
                 this.transform[name] = [args];
             }
-            
+
         }
         clear() {
+            this.step(false);
+            if(this.timeout) clearTimeout(this.timeout);
+            if(this.obj) this.obj.off(transitions[`${Browser}transition`]);
             for (var k in this){
                 this[k] = null;
                 delete this[k];
             }
         }
-        
         /*transform属性*/
         matrix(arg) {
             delete this.transform["matrix3d"];
@@ -242,20 +347,20 @@
         }
         //坐标移动
         translate3d(arg) {
-            arg.forEach((k,i) => {if(!isNaN(k)) arg[i] += "px"});
+            arg.forEach((k, i) => {if(k !== null && !isNaN(k)) arg[i] += "px"});
             this.setTf(arg , "translate3d" , [0 , 0 , 0]);
         }
         translate(arg) {
-            this.translate3d([arg[0], arg[1] , NaN]);
+            this.translate3d([arg[0], arg[1] , null]);
         }
         translateX(arg) {
-            this.translate3d([arg[0] , NaN , NaN]);
+            this.translate3d([arg[0] , null , null]);
         }
         translateY(arg) {
-            this.translate3d([NaN , arg[0] , NaN]);
+            this.translate3d([null , arg[0] , null]);
         }
         translateZ(arg) {
-            this.translate3d([NaN , NaN , arg[0]]);
+            this.translate3d([null , null , arg[0]]);
         }
         //缩放
         scale3d(arg) {
@@ -263,16 +368,16 @@
         }
         scale(arg) {
             if(arg.length === 1) this.scale3d([arg[0] , arg[0]]);
-            else this.scale3d([arg[0] , arg[1] , NaN]);
+            else this.scale3d([arg[0] , arg[1] , null]);
         }
         scaleX(arg) {
-            this.scale3d([arg[0] , NaN , NaN]);
+            this.scale3d([arg[0] , null , null]);
         }
         scaleY(arg) {
-            this.scale3d([NaN , arg[0] , NaN]);
+            this.scale3d([null , arg[0] , null]);
         }
         scaleZ(arg) {
-            this.scale3d([NaN , NaN , arg[0]]);
+            this.scale3d([null , null , arg[0]]);
         }
         //旋转    rotate IE rotate3d(0,1,0,deg) deg>90<270 bug
         rotate3d(arg) {
@@ -297,21 +402,44 @@
         //斜切
         skew(arg){
             if(arg.length === 1) arg = [arg[0] , arg[0]];
-            arg.forEach((k,i) => {if(!isNaN(k)) arg[i] += "deg"});
+            arg.forEach((k,i) => {if(k !== null && !isNaN(k)) arg[i] += "deg"});
             this.setTf(arg , "skew" , [0 , 0]);
         }
         skewX(arg){
             this.skew([arg[0]]);
         }
         skewY(arg){
-            this.skew([NaN , arg[0]]);
+            this.skew([null , arg[0]]);
         }
         //3D样式
         perspective(arg){
             this.setTf(isNaN(arg[0]) ? arg : `${arg[0]}px` , "perspective" , [0]);
         }
+        static getTranslate3d(a){
+            var defaults = $(a)[0].style.transform
+            if (defaults && defaults.indexOf("translate3d") != -1) {
+                defaults = defaults.match(/translate3d\((.*)\)/i)[1].replace(/px/ig, "").split(",");
+                return defaults = {
+                    x : Number(defaults[0]),
+                    y : Number(defaults[1]),
+                    z : Number(defaults[2])
+                };
+            } else if (defaults && defaults.indexOf("matrix") != -1) {
+                defaults = defaults.match(/matrix\((.*)\)/i)[1].replace(/px/ig, "").split(",");
+                return defaults =  {
+                    x : Number(defaults[4]),
+                    y : Number(defaults[5]),
+                    z : 0
+                };;
+            }
+            return defaults =  {
+                    x : 0,
+                    y : 0,
+                    z : 0
+            };;
+        }
     }
-    
+
     /*贝赛尔曲线*/
     $.cssEase = {
         'ease'          : 'ease',
